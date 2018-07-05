df9007cd456648341b6ab6e0675cdacb25bb5d3f
Jerry <jercoinc@yahoo.com>
2018-07-05 16:34:31 -0500

    Complete automatic sprite generation.

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -9,8 +9,8 @@ mixins = require('postcss-mixins');
 gulp.task('styles', function() {
   return gulp.src('./app/assets/styles/styles.css')
     .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
-    .on('error', function(err) {
-      console.log(err.toString());
+    .on('error', function(errorInfo) {
+      console.log(errorInfo.toString());
       this.emit('end');
     })
     .pipe(gulp.dest('./app/temp/styles'));
